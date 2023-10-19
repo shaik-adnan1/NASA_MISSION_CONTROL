c592965fbcbba23f9c3f6b15993318840b523fe6
shaik-adnan1 <yousufwrk0@gmail.com>
2023-10-19 23:09:46 +0530

    imported api data

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,4 +1,41 @@
-const planets = [];
+const { parse } = require("csv-parse");
+const fs = require("fs");
 
+const habitablePlanets = [];
 
-module.exports = planets
+function isHabitable(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+// creating a read stream, that can read csv files
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      // after reading the csv files <->
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", data => {
+    if (isHabitable(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on("error", err => {
+    console.log(err);
+  })
+  .on("end", () => {
+    habitablePlanets.map((curPlanetData, i) => {
+      console.log(`${i + 1}: Planet Name: ${curPlanetData.kepler_name}`);
+    });
+    console.log("Done");
+  });
+// parse();
+
+module.exports = habitablePlanets
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,10 +1,10 @@
-const planets = require('../../models/planets.model.js')
+const planets = require("../../models/planets.model.js");
 
 function getAllPlanets(req, res) {
-     res.status(200).json(planets)
-    console.log('planets')
+  res.status(200).json({ data: planets });
+  console.log("planets");
 }
 
 module.exports = {
-    getAllPlanets
-}
+  getAllPlanets,
+};
