1bea2c6cb987842f6a8f421695036afe3e49d274
shaik-adnan1 <yousufwrk0@gmail.com>
2023-10-20 22:23:24 +0530

    automated parllel script runs with concurrently moved client build to server directory using env variables through package.json

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const morgan = require('morgan')
 const express = require("express");
 const cors = require('cors')
 
@@ -8,7 +10,12 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }))
+
+app.use(morgan('combined'));
+
 app.use(express.json());
+
+app.use(express.static(__dirname, '..', 'public'))
 app.use('/planets', planetsRouter);
 
 
